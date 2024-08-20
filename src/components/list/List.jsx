851aaac19da8ae0536dfb728b3a8446ada458f54
jello. <67309482+jrsnleons@users.noreply.github.com>
2024-08-20 23:23:03 +0800

    fixed build error - List path

--- src/components/list/List.jsx
+++ src/components/list/List.jsx
@@ -1,14 +1,14 @@
+import ChatList from "./chatList/ChatList";
 import "./list.css";
 import UserInfo from "./userInfo/UserInfo";
-import ChatList from "./chatList/chatList";
 
 const List = () => {
   return (
-  <div className="list">
-    <UserInfo/>
-    <ChatList/>
-  </div>
-  )
+    <div className="list">
+      <UserInfo />
+      <ChatList />
+    </div>
+  );
 };
 
 export default List;
